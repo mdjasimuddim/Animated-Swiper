49f5e47f130cc4d93cfa95407e19b57dbce010e4
jasim <jasimuddim231@gmail.com>
2024-02-14 11:58:28 +0600

    Animated Swiper

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ActiveSlider from './Components/ActiveSlider'
+
+const App = () => {
+  return (
+    <div>
+      <ActiveSlider />
+    </div>
+  )
+}
+
+export default App
---
+++ src/data/index.js
@@ -0,0 +1,49 @@
+import {
+    RxCrop,
+    RxDesktop,
+    RxPencil2,
+    RxReader,
+    RxRocket,
+    RxAccessibility
+} from "react-icons/rx"
+
+
+export const ServiceData = [
+    {
+        icon:RxCrop,
+        title:"Development",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        backgroundImage:'https://images.unsplash.com/photo-1706685838669-69d5dcb5ecae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        icon:RxPencil2,
+        title:"Branding",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        backgroundImage:'https://images.unsplash.com/photo-1699059089454-d3e612df3d44?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D'
+    },
+    {
+        icon:RxDesktop,
+        title:"Design",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        backgroundImage:'https://images.unsplash.com/photo-1652557399047-5b28d5d2aee3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D'
+    },
+    {
+        icon:RxReader,
+        title:"Seo",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        backgroundImage:'https://plus.unsplash.com/premium_photo-1667099521634-8fd821e4ed12?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8'
+    },
+    {
+        icon:RxAccessibility,
+        title:"Management",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        backgroundImage:'https://images.unsplash.com/photo-1636867921806-d74911ec31c8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE5fHx8ZW58MHx8fHx8'
+    },
+    {
+        icon:RxRocket,
+        title:"Production",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        backgroundImage:'https://images.unsplash.com/photo-1500990702037-7620ccb6a60a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE3fHx8ZW58MHx8fHx8'
+    }
+
+]
